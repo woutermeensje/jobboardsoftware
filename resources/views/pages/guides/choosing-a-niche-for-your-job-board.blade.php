40d36d11001435e5eacd6d3e23740484472eb298
WouterMeens <w.m.meens@gmail.com>
2026-08-19 08:38:50 +0200

    update

--- resources/views/pages/guides/choosing-a-niche-for-your-job-board.blade.php
+++ resources/views/pages/guides/choosing-a-niche-for-your-job-board.blade.php
@@ -13,7 +13,47 @@
         <h1>Choosing a niche for your job board</h1>
 
         <div class="rich-text">
-          <p>This guide page is ready for content about choosing a niche by industry, region, skill, profession or another focused audience angle.</p>
+          <p>The niche is probably the most important decision you make when you start a job board.</p>
+
+          <p>In our experience, starting a sector- or industry-focused job board, such as construction, nonprofits or finance, is often better than choosing a skill- or profession-based job board, such as Laravel developers or accountants. That does not mean you cannot make money with a profession-based job board. On the contrary. We simply think it is harder to scale, and you need to work harder to build a name.</p>
+
+          <h2>Example</h2>
+
+          <p>Let's say you start a job board focused solely on accountants. The average company may hire only one accountant. Companies often have large sales teams and engineering teams, but most of the time they have only one accountant. So every time a company has an open position for an accountant, the person responsible for finding that accountant and posting the job online must know about your job board.</p>
+
+          <p>If you want to receive 50 paid job postings a year, that means more than 50 people at 50 different companies, all responsible for hiring accountants, should know about your job board. That is not impossible, but it is not easy either.</p>
+
+          <p>Now compare that with a job board focused on the construction industry. Construction is a very large industry with one of the highest numbers of jobs in almost any country. Even in some developed economies, construction still accounts for around 10% of GDP.</p>
+
+          <p>Then look at the average construction company. It is probably continuously in need of carpenters, engineers, planners, office workers, managers, transport staff, support staff and so on.</p>
+
+          <p>Now imagine you have the same goal: getting 50 paid job posts a year on your job board. If you find companies that want to work with you, they are much more likely to purchase multiple job posts on your platform.</p>
+
+          <p>And that is only the start. Because now you can really start working with them. You can make a difference by helping them find candidates and turning them into long-term clients.</p>
+
+          <h2>If you decide to go for a job title or profession-specific job board</h2>
+
+          <p>If you decide to focus your job board on a specific job title, profession or skill set, my advice would be to focus on roles that companies need at a larger scale. For example: sales, marketing, communications, engineering and office roles.</p>
+
+          <p>Just think about it. A decent-sized company almost never has only one salesperson, engineer or online marketer in the marketing department. That makes it a little easier to work with clients.</p>
+
+          <h2>The other problem with job title or profession-based job boards</h2>
+
+          <p>Another big problem I see with job boards that are job title- or profession-specific is that it is harder to create happy customers.</p>
+
+          <p>Job postings do not always generate new hires for a company. That is just the way it is. But consider the accountant job board example again. Think about a company that publishes only one job post, and that job does not generate enough good candidates.</p>
+
+          <p>Now the client is not satisfied because they spent money and basically did not receive a return.</p>
+
+          <p>However, if you work with a company that posts 10 to 50 jobs per year, the equation changes. Maybe out of those 10 jobs, only two generate a hire through your job board. The client pays a subscription of maybe $2,000 per year. Although 80% of the job postings did not generate a direct return, those two hires may have saved the company $15,000 in recruitment fees compared with outsourcing the roles to a recruitment agency.</p>
+
+          <p><strong>Now that is what I call a return.</strong></p>
+
+          <h2>What are good sectors to start a job board in?</h2>
+
+          <p>After running five job boards ourselves, two of which we consider successful, and after speaking to dozens of job board owners whose platforms were for sale, we created a list of industries, sectors and organization types that can be interesting for launching a new job board.</p>
+
+          <p>You can download that list via the link below.</p>
         </div>
       </article>
 
